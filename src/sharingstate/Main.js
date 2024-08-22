6b87c7084adf00e003628ba1f50187a447b1eecf
ptomer40 <ptomer40@yahoo.com>
2024-08-22 13:48:26 +0530

    updated to only on child contenet visible at a time

--- src/sharingstate/Main.js
+++ src/sharingstate/Main.js
@@ -2,15 +2,16 @@ import { useState } from "react";
 import bshake from './bshake.jpg'
 
 
-function MyChild({title,children}){
-const [enable,setEnable]=useState(false);
+function MyChild({title,children,isActive,onShow}){
+
+  
 
 return(
     <>
     <h1>{title}</h1>
-{enable? 
+{isActive? 
 (<p>{children}</p>):(
-    <button onClick={()=>setEnable(true)}>Show</button>
+    <button onClick={onShow}>Show</button>
 )
 } 
 </>
@@ -18,12 +19,12 @@ return(
 
 
 export default function Main(){
-
+const[activeIndex,setActiveIndex]=useState(0);
 return(
     <>
     <h2>BreakFast Food</h2>
     <img src={bshake} height={200} width={200} alt='banana'></img>
-    <MyChild title="BananaShake">
+    <MyChild title="BananaShake" isActive={activeIndex===0} onShow={()=>setActiveIndex(0)}  >
         <p>
 This Banana Milkshake recipe includes sweet bananas and coconut milk for a rich and
  creamy consistency that everyone (especially kids) will love. Plus I add a bit of vanilla and cinnamon for an extra delicious drink that’s perfect for breakfast, as a snack, or even for a healthy dessert. This Banana Shake recipe is also vegan.
@@ -32,7 +33,7 @@ This Banana Milkshake recipe includes sweet bananas and coconut milk for a rich 
         </p>
     </MyChild>
  
- <MyChild title ="MangoShake">
+ <MyChild title ="MangoShake" isActive={activeIndex===1} onShow={()=>setActiveIndex(1)}>
     <p>
 
     A sweet and refreshing mango shake is the perfect drink to enjoy when mangos are in their peak of the season. Here I share two equally easy and delicious recipes (with step-by-step instructions and photos + video) for making rich and creamy mango milkshakes – 
